af71ed9b1ac5687c0763ee6710d0dae0409a8a95
Medet Ahmetson <milayter@gmail.com>
2023-11-30 07:20:15 +0700

    Add LinkedNft

---
+++ packages/hardhat/contracts/LinkedNft.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC721URIStorage } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+/**
+ * @notice WrappedNft is an NFT on the Source Blockchain, which locks the original NFT, and disables transferring it.
+ * When an NFT is wrapped, the Listening oracles would mint the copy on target Blockchain.
+ */
+contract LinkedNft is ERC721URIStorage {
+    address public registrar;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        registrar = msg.sender;
+    }
+
+    // mint and burn are done by the registrar
+}

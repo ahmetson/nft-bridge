e2be533648a1b4aaf8967d85e8662ca26e37ea61
Medet Ahmetson <milayter@gmail.com>
2023-11-30 22:35:10 +0700

    Sort imports in registrar

--- packages/hardhat/contracts/Registrar.sol
+++ packages/hardhat/contracts/Registrar.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import { IRouterClient } from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import { CCIPReceiver } from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import { WrappedNft } from "./WrappedNft.sol";
 import { LinkedNft } from "./LinkedNft.sol";
-import { CCIPReceiver } from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 
 /**
  * A smart contract that allows changing a state variable of the contract and tracking the changes

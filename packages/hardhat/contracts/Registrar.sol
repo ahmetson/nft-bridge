5971040d18b5fd5fa8ea47dace54c5ecaf351af7
Medet Ahmetson <milayter@gmail.com>
2023-12-01 05:52:37 +0700

    Use the local chainlink

--- packages/hardhat/contracts/Registrar.sol
+++ packages/hardhat/contracts/Registrar.sol
@@ -2,9 +2,9 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import { IRouterClient } from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
-import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
-import { CCIPReceiver } from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import { IRouterClient } from "./chainlink/ccip/interfaces/IRouterClient.sol";
+import { Client } from "./chainlink/ccip/libraries/Client.sol";
+import { CCIPReceiver } from "./chainlink/ccip/applications/CCIPReceiver.sol";
 import { WrappedNft } from "./WrappedNft.sol";
 import { LinkedNft } from "./LinkedNft.sol";
 
